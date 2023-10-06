e8410e6e4695e7e2affac5b76ad84114480048f2
Your Name <you@example.com>
2023-10-05 20:07:14 -0400

    base
    side bar base

---
+++ Porfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
